578c7b79a8c25ff1525aa5f2c97154f3b5cd206f
Mike Manchee <mikemanchee@gmail.com>
2020-12-01 11:13:08 -0800

    move other board components to Game to the project
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>
    Co-Authored-By: Joseph Nilles <jbnilles24@gmail.com>

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,7 +12,21 @@ class Game extends React.Component {
       xIsNext: true,
     };
   }
-
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
   render() {
     const history = this.state.history;
     const current = history[history.length - 1]
